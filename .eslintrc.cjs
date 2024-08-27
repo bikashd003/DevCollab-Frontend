422a1f1c68affc5f8296649f44ca266caae29dc3
Bikash Das <bikashd003@gmail.com>
2024-08-27 13:08:11 +0530

    ...

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'prettier/prettier': 'error',
+    'no-console': 'error', // This will cause ESLint to error on console.log
+    'react/react-in-jsx-scope': 'off', // Not needed in Next.js
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
     ],
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
