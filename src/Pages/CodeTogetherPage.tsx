ecde87a5288bbd08f34d6fc5d1f027c91b085889
Bikash Das <bikashd003@gmail.com>
2024-11-23 00:35:52 +0530

    fixing some render and socket

--- src/Pages/CodeTogetherPage.tsx
+++ src/Pages/CodeTogetherPage.tsx
@@ -1,26 +1,40 @@
 import { motion } from 'framer-motion';
 import { IoMdSend } from 'react-icons/io';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import CollaborativeEditor from './CollaborativeEditor';
 import { useParams } from 'react-router-dom';
 import { Share } from 'lucide-react';
 import { socket } from '../Utilities/Socket';
-import { Avatar } from 'antd';
+import { Avatar, message } from 'antd';
 import { useAuth } from '../Secure/AuthContext';
 
 const CodeTogetherPage = () => {
-  const [messages, setMessages] = useState<Array<{ id: number; user: string; text: string }>>([]);
+  const [messages, setMessages] = useState<
+    Array<{
+      username: { _id: string; username: string };
+      message: string;
+      timestamp: Date;
+    }>
+  >([]);
   const [newMessage, setNewMessage] = useState('');
+  const [typingUsers, setTypingUsers] = useState<string[]>([]);
   const [connectedUsers, setConnectedUsers] = useState<
     Array<{ username: string; profilePicture: string }>
   >([]); // Real-time connected users
   const { id } = useParams();
   const [shareableLink, setShareableLink] = useState('');
   const { currentUserId } = useAuth();
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
   // Initialize socket and manage real-time events
   useEffect(() => {
     socket.connect();
-    // Join the project room
+
     if (id && currentUserId) {
       socket.emit('joinProject', { projectId: id, userId: currentUserId });
       setShareableLink(`${window.location.origin}/editor/${id}`);
@@ -31,18 +45,35 @@ const CodeTogetherPage = () => {
       setMessages(prev => [...prev, message]);
     });
 
+    socket.on('userTyping', (username: string) => {
+      setTypingUsers(prev => [...new Set([...prev, username])]);
+    });
+
+    socket.on('userStoppedTyping', (username: string) => {
+      setTypingUsers(prev => prev.filter(user => user !== username));
+    });
+
     // Update the list of connected users
-    socket.on('connectedUsers', (users: Array<{ username: string; profilePicture: string }>) => {
+    socket.on('connectedUsers', users => {
       setConnectedUsers(users);
+      message.success(`${users.length} users connected`);
+    });
+
+    socket.on('chatHistory', history => {
+      if (history && history.chatHistory) {
+        setMessages(history.chatHistory);
+      }
     });
 
     return () => {
       socket.off('chatMessage');
       socket.off('connectedUsers');
+      socket.off('chatHistory');
+      socket.off('userTyping');
+      socket.off('userStoppedTyping');
       socket.disconnect();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [id]);
+  }, [currentUserId, id]);
 
   const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -60,7 +91,21 @@ const CodeTogetherPage = () => {
 
     setNewMessage('');
   };
+  let typingTimeout: ReturnType<typeof setTimeout>;
+  const handleTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMessage(e.target.value);
 
+    // Emit typing event
+    socket.emit('typing', { projectId: id });
+
+    // Clear existing timeout
+    clearTimeout(typingTimeout);
+
+    // Set new timeout to emit stopped typing
+    typingTimeout = setTimeout(() => {
+      socket.emit('stopTyping', { projectId: id });
+    }, 1000);
+  };
   return (
     <div className="flex flex-col h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-gray-900 dark:text-gray-100">
       <main className="flex-grow flex flex-col lg:flex-row overflow-hidden p-4 gap-4">
@@ -73,16 +118,16 @@ const CodeTogetherPage = () => {
         <div className="w-full lg:w-1/3 flex flex-col rounded-2xl backdrop-blur-sm bg-white/80 dark:bg-gray-800/80 shadow-xl">
           {/* Chat Section */}
           <div className="p-4 border-b flex gap-2 justify-between border-gray-200 dark:border-gray-700">
-            <div>
+            <div className="flex items-center gap-2">
               <h2 className="text-xl font-semibold">Chat</h2>
-              {connectedUsers.map(user => (
-                <p key={user.username} className=" flex items-center gap-2">
-                  {user.profilePicture ? (
-                    <Avatar size={28} src={user.profilePicture} />
+              {connectedUsers?.map(user => (
+                <Avatar.Group key={user?.username}>
+                  {user?.profilePicture ? (
+                    <Avatar size={28} src={user?.profilePicture} />
                   ) : (
-                    <Avatar size={28}>{user.username.charAt(0).toUpperCase()}</Avatar>
+                    <Avatar size={28}>{user?.username?.charAt(0).toUpperCase()}</Avatar>
                   )}
-                </p>
+                </Avatar.Group>
               ))}
             </div>
             <Share
@@ -90,34 +135,46 @@ const CodeTogetherPage = () => {
               onClick={() => navigator.clipboard.writeText(shareableLink)}
             />
           </div>
-          <div className="flex-grow overflow-y-auto p-4 space-y-4 scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600">
-            {messages.map((message: { id: number; user: string; text: string }) => (
-              <motion.div
-                key={message.id}
-                className={`max-w-[80%] ${message.user === 'You' ? 'ml-auto' : ''}`}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3 }}
-              >
-                <div
-                  className={`p-3 rounded-2xl ${
-                    message.user === 'You'
-                      ? 'bg-indigo-500 text-white'
-                      : 'bg-gray-200 dark:bg-gray-700'
-                  }`}
+          <div className="flex-grow overflow-y-auto p-4 space-y-4 scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-600 scroll-smooth">
+            {messages?.map(
+              (message: {
+                username: { _id: string; username: string };
+                message: string;
+                timestamp: Date;
+              }) => (
+                <motion.div
+                  key={message?.username?._id}
+                  className={`max-w-[80%] ${message?.username?._id === currentUserId ? 'ml-auto' : ''}`}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.3 }}
                 >
-                  <p className="text-sm font-medium mb-1">{message.user}</p>
-                  <p className="text-sm">{message.text}</p>
-                </div>
-              </motion.div>
-            ))}
+                  <div
+                    className={`p-3 rounded-2xl ${
+                      message?.username?._id === currentUserId
+                        ? 'bg-indigo-500 text-white'
+                        : 'bg-gray-200 dark:bg-gray-700'
+                    }`}
+                  >
+                    <p className="text-sm font-medium mb-1">{message?.username?.username}</p>
+                    <p className="text-sm">{message?.message}</p>
+                  </div>
+                </motion.div>
+              )
+            )}
+            {typingUsers.length > 0 && (
+              <div className="text-sm text-gray-500 italic">
+                {typingUsers.join(', ')} {typingUsers.length === 1 ? 'is' : 'are'} typing...
+              </div>
+            )}
+            <div ref={messagesEndRef} />
           </div>
           <div className="p-4 border-t border-gray-200 dark:border-gray-700">
             <form onSubmit={handleSendMessage} className="flex gap-2">
               <input
                 type="text"
                 value={newMessage}
-                onChange={e => setNewMessage(e.target.value)}
+                onChange={handleTyping}
                 placeholder="Type a message..."
                 className="flex-grow px-4 py-2 rounded-full bg-gray-100 dark:bg-gray-700 border-none focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:focus:ring-indigo-400 transition-all"
               />
